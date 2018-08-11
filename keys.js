c684ac4be560a55b6a7bb3709a42113a4e3dc09a
BogieBogard <mbogard@utexas.edu>
2018-08-10 21:18:26 -0500

    Added keys and required the keys.js and .env in liri.js

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.bXJG1ouIUlmd9OQBoUv3OXqX6,
+  consumer_secret: process.env.o9G4Mzy8PA7jZwqrHriPzo2Anv2JWKTCqAq0UCPayyBM0R2rz5,
+  access_token_key: process.env.IYKJCwpCiPNQUe5iSc9ErBSDWyrjz,
+  access_token_secret: process.env.azDqX7NAWPHdYffxXjZfGbrEJooHxMego02ruwEWCgNIq
+};
+
+exports.spotify = {
+  id: process.env.e02c7a83c53d4c6d9169674f66e0eb38,
+  secret: process.env.dd8170e41714d13955b6fd444df4716
+};
+
+exports.test = {
+    id: "This is a test"
+}
---
+++ liri.js
@@ -0,0 +1,7 @@
+
+let myKeys = require("./keys.js");
+
+console.log(myKeys.test);
+console.log("test2");
+
+require("./.env").config();
